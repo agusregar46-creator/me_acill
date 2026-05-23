8d162da8f217ba02ae0fd98b49907be7249dad52
agusregar46 <agusregar46@gmail.com>
2026-05-23 12:32:28 +0700

    fix app routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,67 +2,21 @@ import {
   BrowserRouter,
   Routes,
   Route,
-  Link,
 } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Admin from "./pages/Admin";
 import Dashboard from "./pages/Dashboard";
-import AdminRoute from "./components/AdminRoute";
+import Admin from "./pages/Admin";
 import About from "./pages/About";
 
-import { useAuth } from "./hooks/useAuth";
-
 export default function App() {
-  const { user, logout } = useAuth();
-
   return (
     <BrowserRouter>
-      <nav className="flex items-center justify-between border-b border-slate-800 bg-slate-950 px-8 py-4 text-white">
-  <div className="text-2xl font-bold text-purple-400">
-    AcillNet 🚀
-  </div>
-
-  <div className="flex items-center gap-6">
-    <Link to="/">Home</Link>
-
-    {!user ? (
-      <>
-        <Link to="/login">
-          Login
-        </Link>
-
-        <Link to="/register">
-          Register
-        </Link>
-      </>
-    ) : (
-      <>
-        <span className="text-slate-300">
-          {user.email}
-        </span>
-
-        <button
-          onClick={logout}
-          className="rounded-lg bg-red-500 px-4 py-2 hover:bg-red-600"
-        >
-          Logout
-        </button>
-      </>
-    )}
-
-    <Link
-      to="/admin"
-      className="rounded-lg bg-purple-600 px-4 py-2 hover:bg-purple-700"
-    >
-      Admin
-    </Link>
-  </div>
-</nav>
 
       <Routes>
+
         <Route
           path="/"
           element={<Home />}
@@ -78,24 +32,23 @@ export default function App() {
           element={<Register />}
         />
 
-       <Route
-  path="/admin"
-  element={
-    <AdminRoute>
-      <Admin />
-    </AdminRoute>
-  }
-/>
         <Route
           path="/dashboard"
           element={<Dashboard />}
         />
+
+        <Route
+          path="/admin"
+          element={<Admin />}
+        />
+
+        <Route
+          path="/about"
+          element={<About />}
+        />
+
       </Routes>
-      <Route
-  path="/about"
-  element={<About />}
-/>
-</route>
+
     </BrowserRouter>
   );
 }
